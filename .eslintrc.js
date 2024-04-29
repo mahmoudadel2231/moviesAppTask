48df3ba289b892402e9987363b17fafbada8eb2c
mahmoudadel2231 <mahmoudelsehemy97@gmail.com>
2024-04-29 21:05:30 +0300

    initial commit Add typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,65 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: [
+    '@react-native-community',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['@typescript-eslint', 'import'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [
+      1,
+      {extensions: ['.js', '.jsx', '.ts', '.tsx']},
+    ],
+    'no-undef': 'off',
+    'no-shadow': 'off',
+    'react/jsx-uses-react': 1,
+    'react/jsx-uses-vars': 1,
+    'no-console': ['error'],
+    curly: [2, 'multi-line'],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['parent', 'sibling'],
+          'index',
+          'object',
+          'type',
+        ],
+        pathGroups: [
+          {
+            pattern: 'react+(|-native)',
+            group: 'external',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+    '@typescript-eslint/consistent-type-imports': 'error',
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': 'babel-module',
+    'import/ignore': ['node_modules/react-native/index\\.js$', 'react-native'],
+  },
 };
